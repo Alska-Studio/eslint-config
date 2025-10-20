7a5d6c4add6f1ec4328b29229031bfbb41c1259a
Peter <peter@alska.studio>
2025-10-20 13:48:30 +0200

    feat: Extract packages repositories and use as submodules (GLM-1704)

---
+++ index.js
@@ -0,0 +1,34 @@
+const semistandard = require('eslint-config-semistandard')
+
+module.exports = {
+  semistandard,
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended',
+    'prettier',
+    'turbo'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte']
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser'
+      }
+    }
+  ]
+};
